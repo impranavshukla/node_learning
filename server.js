316c41f262dfd73f32364a04ed892950893185e7
Pranav Shukla <impranavshukla@gmail.com>
2025-04-04 14:15:46 +0530

    shift mongodb to online

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express();
 const db=require('./db');
+require('dotenv').config();
 
 const bodyParser=require('body-parser');
 app.use(bodyParser.json());
+const PORT=process.env.PORT || 3000;
 
 const Person=require('./models/Person');
 const MenuItem=require('./models/MenuItem');
@@ -37,6 +39,9 @@ const personRoutes=require('./routes/personRoutes');
 //use the router files
 app.use('/person',personRoutes);
 
-app.listen(3000,()=>{
+
+
+
+app.listen(PORT,()=>{
     console.log('Server is running on port 3000')
 })
